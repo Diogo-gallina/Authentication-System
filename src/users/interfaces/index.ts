b348f8b2a4ca5c1d4a6b508998a2809765484364
Diogo-gallina <diogogallina20@gmail.com>
2023-07-12 19:32:00 -0300

    feat: create-list-user-interface

--- src/users/interfaces/index.ts
+++ src/users/interfaces/index.ts
@@ -1,2 +1,5 @@
 export * from './register-user';
 export * from './user-repository';
+export * from './delete-user';
+export * from './list-user';
+export * from './register-use-case-response';
---
+++ src/users/interfaces/list-user.ts
@@ -0,0 +1,3 @@
+export class IListUser {
+  id: string;
+}
