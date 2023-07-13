99aa2cabce1dbf6d984ac03d9057fe5fe01dafec
Diogo-gallina <diogogallina20@gmail.com>
2023-07-13 13:03:05 -0300

    refactor: remove-expots-to-list/delete-interfaces

--- src/users/dto/index.ts
+++ src/users/dto/index.ts
@@ -1,2 +1,1 @@
 export * from '@/users/dto/register-user.dto';
-export * from '@/users/dto/delete-user.dto';
--- src/users/interfaces/index.ts
+++ src/users/interfaces/index.ts
@@ -1,5 +1,3 @@
 export * from './register-user';
 export * from './user-repository';
-export * from './delete-user';
-export * from './list-user';
 export * from './register-use-case-response';
