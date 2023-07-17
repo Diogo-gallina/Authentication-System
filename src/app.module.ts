0150340ec9e8c30992391ff564d917503e07781c
Diogo-gallina <diogogallina20@gmail.com>
2023-07-17 17:15:40 -0300

    feat: create-token-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TokenModule } from './token/token.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, TokenModule],
 })
 export class AppModule {}
