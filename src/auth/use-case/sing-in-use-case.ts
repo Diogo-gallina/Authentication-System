723dbd6a3ee4a558676a79a706bf1da752a8d1b6
Diogo-gallina <diogogallina20@gmail.com>
2023-07-17 09:29:28 -0300

    feat: create- jwt-magic-const

--- src/auth/use-case/sing-in-use-case.ts
+++ src/auth/use-case/sing-in-use-case.ts
@@ -1,4 +1,5 @@
 import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 import { UsersRepository } from '@/users/infra/repositories';
 import { ISingInUseCase } from '../interface/sing-in-use-case';
@@ -6,12 +7,12 @@ import { INVALID_CREDENTIALS_ERROR } from '@/shared/constants/erros';
 import { ISingInUseCaseResponse } from '../interface/sing-in-use-case-response';
 
 export class SingInUseCase {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(
+    private usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
 
-  async execute({
-    email,
-    password,
-  }: ISingInUseCase): Promise<ISingInUseCaseResponse> {
+  async execute({ email, password }: ISingInUseCase) {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) throw new Error(INVALID_CREDENTIALS_ERROR);
@@ -25,8 +26,10 @@ export class SingInUseCase {
 
     if (!doesPasswordMatches) throw new Error(INVALID_CREDENTIALS_ERROR);
 
+    const payload = { sub: user.id, email: user.email };
+
     return {
-      user,
+      access_token: await this.jwtService.signAsync(payload),
     };
   }
 }
