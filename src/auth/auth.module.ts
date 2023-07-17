c1fa49d58df6c82e5b83026d4efb319fbac2ede3
Diogo-gallina <diogogallina20@gmail.com>
2023-07-17 09:37:56 -0300

    feat: create-authController

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,29 @@
-// import { Module } from '@nestjs/common';
-// import { AuthController } from '../shared/infra/http/controllers/auth.controller';
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
-// @Module({
-//   controllers: [AuthController],
-//   providers: [],
-// })
-// export class AuthModule {}
+import { UsersModule } from '../users/users.module';
+import { AuthController } from '@/shared/infra/http/controllers';
+import { jwtConstants } from '@/shared/constants/constants';
+import { SingInUseCase } from './use-case/sing-in-use-case';
+import { IUsersRepository } from '@/users/interfaces';
+import { UsersRepository } from '@/users/infra/repositories';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    SingInUseCase,
+    {
+      provide: IUsersRepository,
+      useClass: UsersRepository,
+    },
+  ],
+  controllers: [AuthController],
+})
+export class AuthModule {}
